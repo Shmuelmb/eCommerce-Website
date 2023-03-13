45cd4b23a5024627d433c902ea1ddb7e2a803d37
unknown <shmuel.cohen0@gmail.com>
2023-03-13 22:41:38 +0200

    add sort in category page

--- client/src/components/Pages/CategoryPage/CategoryPage.js
+++ client/src/components/Pages/CategoryPage/CategoryPage.js
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import ShoppingBarTools from "../EnterPage/ShoppingBarTools/ShoppingBarTools";
 import "./CategoryPage.css";
 import ProductCard from "./ProductCard/ProductCard";
 const CategoryPage = () => {
@@ -25,21 +26,35 @@ const CategoryPage = () => {
   return (
     list && (
       <div className="CategoryPage">
-        <h1>{Category}'S CLOTHES</h1>
+        <h1>{Category}'S COLLECTION</h1>
+        <div className="products-container">
+          <ShoppingBarTools />
+          <div className="p">
+            <div className="products-sort">
+              <select>
+                <option disabled={true} selected={true}>
+                  SORT
+                </option>
+                <option>HIGH TO LOW</option>
+                <option>LOW TO HIGH</option>
+              </select>
+            </div>
 
-        <div className="productss">
-          {list.map((event, index) => {
-            return (
-              <ProductCard
-                category={event.category}
-                key={index + 10}
-                Urlimage={event.goods_img}
-                id={event._id}
-                title={event.goods_name}
-                price={event.retailPrice.amount}
-              />
-            );
-          })}
+            <div className="products-list">
+              {list.map((event, index) => {
+                return (
+                  <ProductCard
+                    category={event.category}
+                    key={index + 10}
+                    Urlimage={event.goods_img}
+                    id={event._id}
+                    title={event.goods_name}
+                    price={event.retailPrice.amount}
+                  />
+                );
+              })}
+            </div>
+          </div>
         </div>
       </div>
     )
--- client/src/components/Pages/CategoryPage/ProductCard/ProductCard.js
+++ client/src/components/Pages/CategoryPage/ProductCard/ProductCard.js
@@ -3,10 +3,6 @@ import Addbtn from "../../EnterPage/Products/Product/btns/Addbtn.js";
 import { useNavigate } from "react-router-dom";
 import { useContext } from "react";
 import MyContext from "../../../../MyContext.js";
-import Card from "@mui/material/Card";
-import Typography from "@mui/material/Typography";
-import Stack from "@mui/material/Stack";
-import CardContent from "@mui/material/CardContent";
 import Zoom from "@mui/material/Zoom";
 import "./ProductCard.css";
 const ProductCard = ({ id, Urlimage, title, price }) => {
@@ -14,32 +10,22 @@ const ProductCard = ({ id, Urlimage, title, price }) => {
   const { setProductID } = useContext(MyContext);
   return (
     <Zoom in={true}>
-      <Card className="product-card" sx={{ width: 250, height: 400 }}>
-        <Stack>
-          <div
-            className="card-hover"
-            onClick={() => {
-              navigate(`/products/${id}`);
-              setProductID(id);
-            }}
-          >
-            <Addbtn id={id} />
-          </div>
-
-          <img className="cardImagee" src={Urlimage} alt={title} />
-          <CardContent>
-            <Typography
-              className="ty"
-              gutterBottom
-              variant="body2"
-              color="text.secondary"
-            >
-              {title}
-            </Typography>
-            <Typography color="black">${price}</Typography>
-          </CardContent>
-        </Stack>
-      </Card>
+      <div className="product-card">
+        <div
+          className="card-hover"
+          onClick={() => {
+            navigate(`/products/${id}`);
+            setProductID(id);
+          }}
+        >
+          <Addbtn id={id} />
+        </div>
+        <img className="cardImage" src={Urlimage} alt={title} />
+        <div className="card-content">
+          <span className="ty">{title}</span>
+          <span> ${price}</span>
+        </div>
+      </div>
     </Zoom>
   );
 };
