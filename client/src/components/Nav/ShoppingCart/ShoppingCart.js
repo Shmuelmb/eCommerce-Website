e08dba5717b4526ae574662f29bc7eb5bfb201cd
unknown <shmuel.cohen0@gmail.com>
2023-03-21 19:39:02 +0200

    add syntax change

--- client/src/components/Nav/ShoppingCart/ShoppingCart.js
+++ client/src/components/Nav/ShoppingCart/ShoppingCart.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import MyContext from "../../../MyContext";
 import "./ShoppingCart.css";
 import Button from "@mui/material/Button";
