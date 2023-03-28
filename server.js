ba18644de6f26474b785da4d1f999e865b498d81
unknown <shmuel.cohen0@gmail.com>
2023-03-28 16:31:35 +0300

    fix dirname bug

--- server.js
+++ server.js
@@ -19,6 +19,11 @@ import {
 import { validateToken } from "./Users/JWT.js";
 import cookiesMiddleware from "universal-cookie-express";
 import { productsByCategoryController } from "./SheinProducts/SheinServices.js";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 //dotenv
 dotenv.config();
@@ -92,7 +97,9 @@ app.get("/api/users/profile", validateToken, profileController);
 // });
 
 //build route
-app.get("*", (req, res) => res.sendFile(__dirname + "/client/dist/index.html"));
+app.get("*", (req, res) => {
+  res.sendFile(__dirname + "/client/dist/index.html");
+});
 
 mongoose.connect(
   `mongodb+srv://${DB_USER}:${DB_PASS}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority`,
